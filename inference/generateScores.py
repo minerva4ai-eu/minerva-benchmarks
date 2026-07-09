944028f526151aedce87d9d82e3e0f3f8e112020
guillaumesalle444 <github@soulty.fr>
2026-07-09 15:21:23 +0200

    Correct model type map path

--- inference/generateScores.py
+++ inference/generateScores.py
@@ -28,9 +28,7 @@ GPUS_PER_NODE = os.getenv("GPUS_PER_NODE", "Add to .env file")
 PARTITION_NAME = os.getenv("PARTITION_NAME", "Add to .env file")
 MACHINE = os.getenv("MACHINE", "Add to .env file")
 MACHINE_TYPE = os.getenv("MACHINE_TYPE", "Add to .env file")
-MODEL_TYPE_MAP = json.load(
-    open(os.path.join(BASE_DIR, "configs", "model_type_map.json"))
-)
+MODEL_TYPE_MAP = json.load(open(os.path.join(BASE_DIR, f"configs-{MACHINE}", "model_type_map.json")))
 if "leonardo" in hostname:
     MODEL_TYPE_MAP = json.load(
         open(os.path.join(BASE_DIR, f"configs-{SUPCOMPUTER_NAME}", "model_type_map.json"))
